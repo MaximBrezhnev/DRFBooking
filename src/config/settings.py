5d00bb86f4cd8e3d19ac1dc2fdc941f93efa8f1d
Your Name <max.b04.03@gmail.com>
2025-07-30 12:47:32 +0300

    dev: Добавлены настройки `dyanconf`

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from dynaconf import settings as settings_
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ft4h5hnyk*^=7r6(4ehoawf60r^jzq1z&&-1*svw42g4*_-7u="
+SECRET_KEY = settings_.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = settings_.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = settings_.get("ALLOWED_HOSTS")
 
 
 # Application definition
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "src.room",
+    "src.booking",
 ]
 
 MIDDLEWARE = [
@@ -49,7 +53,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "src.urls"
+ROOT_URLCONF = "src.config.urls"
 
 TEMPLATES = [
     {
@@ -74,8 +78,12 @@ WSGI_APPLICATION = "src.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": settings_.get("DB_NAME"),
+        "USER": settings_.get("DB_USER"),
+        "PASSWORD": settings_.get("DB_PASSWORD"),
+        "HOST": settings_.get("DB_HOST"),
+        "PORT": settings_.get("DB_PORT"),
     }
 }
 
